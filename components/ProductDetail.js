c2d32d5ff9b4a08f8599986d89676d132a46e558
Ceci-Aguilera <ceci.aguilera@zohomail.com>
2021-12-10 15:06:32 -0500

    Border of color select fixed

--- components/ProductDetail.js
+++ components/ProductDetail.js
@@ -209,7 +209,7 @@ const ProductDetail = ({ product }) => {
                                                 sel_outline = '3px solid royalblue'
                                             }
                                             else {
-                                                sel_outline = '1px solid white'
+                                                sel_outline = '1px solid black'
                                             }
                                             return (
                                                 <Col key={index} xs={4} sm={4} md={3} lg={2}>
