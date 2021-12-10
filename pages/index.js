39685a544674dc0202c06698ef160ed29f4aca2c
Ceci-Aguilera <ceci.aguilera@zohomail.com>
2021-12-10 04:37:43 -0500

    Collection name fixed minor

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ export default function Home() {
         </Row>
 
         <CollectionCarousel collection={'Luxury'} />
-        <CollectionCarousel collection={'Etink'} />
+        <CollectionCarousel collection={'Etnik'} />
         <CollectionCarousel collection={'Cittadino'} />
 
         <CollectionCarousel collection={'Navy'} />
